a54513e41179a50a26a7962e8c87eaff9c5f4286
dodocha169 <dodocha1524@icloud.com>
2021-06-17 16:45:41 +0900

    fixed customIndex.html

--- Sources/EquipmentDatabase/DatabaseScreen.swift
+++ Sources/EquipmentDatabase/DatabaseScreen.swift
@@ -22,31 +22,33 @@ extension TailwindStyle: ClosswiftCombinable {
 
 struct HeaderView: View {
     
+    @State var color: Tailwind.BackgroundColor = .blue(.n200)
+    
     var style: TailwindStyle {
         TailwindStyle.init(
             flexbox: .init(
                 direction: .row,
                 wrap: nil,
-                grow: nil,
+                grow: .enable,
                 shrink: nil,
                 flex: .auto,
                 order: nil),
-            boxAlignment: .init(),
-            sizing: .init(width: .full, height: .n(40)),
+            boxAlignment: .init(justifyContent: .center, justifyItems: .center, justifySelf: .auto, alignContent: .center, alignItems: nil, alignSelf: nil, placeContent: nil, placeItems: nil, placeSelf: nil),
+            sizing: .init(width: .max, height: .n(40)),
             spacing: .init(),
-            background: .init(backgroundColor: .blue(.n100)))
+            background: .init(backgroundColor: color))
     }
     
     public var body: some View {
         html("div", attributes: ["class" :
                                     closs(style, "flex")]) {
-          //  html("img",
-          //       attributes: [
-          //          "src":"https://res.cloudinary.com/pando-life/image/upload/f_auto,b_rgb:E9E9E9,c_limit,w_1260,h_780,q_auto:good/article/main/2jh6rj0kc7jdxymf1591479098.jpg",
-          //          "class": closs(headerLogoStyle)
-          //       ] )
+            html("img",
+                 attributes: [
+                    "src":"https://alienwarezone.jp/images/upload/2021/02/5575be14b51d9eddb2687e215c1e4498.png",
+                    "class": closs(headerLogoStyle)
+                 ] )
             html("div", attributes: ["class" :
-                                        closs(style, "font-sans","text-6xl","pt-11")], content: "三種のチーズ牛丼特盛温玉乗せ")
+                                        closs(style, "font-sans","text-5xl","pt-11")], content: "FFXIV-Equipment Database(仮)")
         }
     }
 }
@@ -57,7 +59,7 @@ let headerLogoStyle = TailwindStyle(
             
         ],
         margin: [
-            .left(40)
+            .left(48)
         ])
 )
 
@@ -70,7 +72,7 @@ let rootViewStyle = TailwindStyle(
         alignContent: nil,
         alignItems: .stretch,
         alignSelf: nil),
-    sizing: .init(width: .screen, height: .screen))
+    sizing: .init(width: .full, height: .full))
 
 struct ContentView: View {
     var contentStyle: TailwindStyle {
@@ -79,14 +81,13 @@ struct ContentView: View {
             flexbox: .init(
                 direction: .col,
                 wrap: nil,
-                grow: .enable,
+                grow: nil,
                 shrink: nil,
                 flex: .enable,
                 order: nil),
             boxAlignment: .init(justifyContent: nil, justifyItems: nil, justifySelf: nil, alignContent: nil, alignItems: nil, alignSelf: nil, placeContent: nil, placeItems: nil, placeSelf: nil),
             sizing: .init(width: .screen, height: .full),
             spacing: .init(margin: [.left(56),.top(-60)]),
-            typography: .init(fontFamily: .sans, fontSize: .xl, fontStyle: .italic, fontSmoothing: nil, fontWeight: .bold, fontVariantNumeric: nil, textAlign: .left, textColor: .gray(.n900), textDecoration: .underline, textOpacity: .n(100), textOverflow: .overflowClip, textTransform: nil, verticalAlign: nil, whitespace: .normal, wordBreak: .words, letterSpacing: .normal, lineHeight: nil, listStyleType: nil, listStylePosition: .inside, placeholderColor: nil, placeholderOpacity: nil),
             background: .init(backgroundColor: .pink(.n100)))
     }
     
@@ -101,7 +102,7 @@ struct ContentView: View {
                 order: nil),
             boxAlignment: .init(justifyContent: .start, justifyItems: .start, justifySelf: nil, alignContent: .start, alignItems: nil, alignSelf: nil, placeContent: nil, placeItems: nil, placeSelf: nil),
             sizing: .init(width: .n(56), height: .full),
-            spacing: .init(margin: [.left(4)]),
+            spacing: .init(),
             background: .init(backgroundColor: .indigo(.n500)))
     }
     
@@ -115,24 +116,50 @@ struct ContentView: View {
                 flex: .auto,
                 order: nil),
             boxAlignment: .init(),
-            sizing: .init(width: .full, height: .n(48)),
-            spacing: .init(),
-            transforms: .init(transform: .transform, transformOrigin: nil, scale: .number(150), rotate: .number(180), translate: nil, skew: .yNumber(12)))
+            sizing: .init(width: .full, height: .auto),
+            spacing: .init())
         
+    }
+    var equipmentStyle: TailwindStyle {
+        TailwindStyle.init(
+            layout: .init(
+            container: .container,
+            boxDecorationBreak: nil,
+            boxSizing: .content,
+            display: .flex,
+            floats: .left,
+                clear: nil,
+                isolation: nil,
+                objectFit: nil,
+                objectPosition: nil,
+                overflow: nil,
+                overscrollbehavior: nil,
+                position: nil,
+                visibility: nil,
+                zIndex: nil,
+                layoutPosition: nil),
+            flexbox: .init(
+                direction: .row,
+                wrap: nil,
+                grow: nil,
+                shrink: nil,
+                flex: .enable,
+                order: nil),
+                grid: .init(),
+                boxAlignment: .init(),
+                sizing: .init(width: .n(60), height: .n(60)),
+                spacing: .init(margin: [.left(0),.top(0)]),
+                typography: .init(),
+                background: .init(backgroundColor: .gray(.n100)))
     }
     public var body: some View {
         html("div", attributes: ["class" :
                                     closs(style)]) {
-          //  html("img",                attributes: [                    "src":"https://res.cloudinary.com/pando-life/image/upload/f_auto,b_rgb:E9E9E9,c_limit,w_1260,h_780,q_auto:good/article/main/2jh6rj0kc7jdxymf1591479098.jpg",
-         //                                                               "class": closs(imageStyle)])
-         //   html("div", attributes: [:], content: "なんもしたくない プロフィールとかかきたい")
-           // html("div", attributes: ["class" :
-       //                                 closs(contentStyle)]) {
-          //      html("a",attributes: ["href":"https://www.sukiya.jp/menu/in/gyudon/102900/index.html","class": closs(contentStyle, "font-sans","m-8")], content: "みんな大好き！")
-          //      html("a",attributes: ["href":"https://www.monsterenergy.com/jp/ja/products/monster-energy","class": closs(contentStyle, "font-sans","m-8")], content: "魔剤しか勝たん")
-         //       html("a",attributes: [:],content: "改行ってどうやるの？")
-            }
-                                    }
+            html("div", attributes: [:], content: "なんもしたくない プロフィールとかかきたい")
+            html("div", attributes: ["class" :
+                                        closs(contentStyle)]) {}
+                                        html("div", attributes: ["class":closs(equipmentStyle)])
+        }
     }
     
     
@@ -155,3 +182,4 @@ public struct HelloView: View {
         }
     }
 }
+
